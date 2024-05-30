fc7f1a542cd5e00f74dfb0eb08e9b2f8c2bf1489
David <dabelesp2@gmail.com>
2024-05-30 13:23:23 +0200

    Changes on hardcodes and unused imports

--- app/src/main/java/com/davidbelesp/mybookshelf/fragments/DatabaseFragment.java
+++ app/src/main/java/com/davidbelesp/mybookshelf/fragments/DatabaseFragment.java
@@ -1,24 +1,11 @@
 package com.davidbelesp.mybookshelf.fragments;
 
-import android.Manifest;
 import android.app.AlertDialog;
-import android.app.DownloadManager;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
-import android.os.Environment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,25 +14,13 @@ import android.widget.Toast;
 
 import com.davidbelesp.mybookshelf.R;
 import com.davidbelesp.mybookshelf.database.BooksDB;
-import com.davidbelesp.mybookshelf.utils.Constants;
 import com.davidbelesp.mybookshelf.utils.PdfUtils;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
-import java.util.Objects;
-import java.util.zip.Inflater;
 
 public class DatabaseFragment extends Fragment {
 
     private Button clearDB;
-    private Button downloadDB;
     private Button downloadPDF;
 
     @Override
@@ -65,27 +40,22 @@ public class DatabaseFragment extends Fragment {
 
         clearDB.setOnClickListener( e -> {
             new AlertDialog.Builder(getContext())
-                    .setTitle("Delete database")
-                    .setMessage("Are you sure you want to delete all entries?")
+                    .setTitle(R.string.delete_database_dialog_title)
+                    .setMessage(R.string.delete_database_dialog_message)
                     .setPositiveButton(android.R.string.yes, (dialog, which) -> BooksDB.getInstance(getContext()).removeAllBooks())
                     .setNegativeButton(android.R.string.no, (dialog, which) -> {})
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .show();
         });
 
-        downloadDB.setOnClickListener( e -> {
-            Toast.makeText(getContext(), "NOT YET IMPLEMENTED", Toast.LENGTH_SHORT).show();
-            //TODO DOWNLOAD DATABASE
-        });
-
         downloadPDF.setOnClickListener( e -> {
+            Toast.makeText(requireContext(), R.string.downloading_pdf, Toast.LENGTH_SHORT).show();
             this.createAndOpenPdf();
         });
     }
 
     private void loadElements(View view) {
         this.clearDB = view.findViewById(R.id.buttonDeleteDB);
-        this.downloadDB = view.findViewById(R.id.buttonDownloadDB);
         this.downloadPDF = view.findViewById(R.id.buttonDownloadPDF);
     }
 
